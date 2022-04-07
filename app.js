96564bcec503ddc01e824bcc9b3d756946ab19cf
Daniel Chang <daniel.changk@gmail.com>
2022-04-07 11:44:16 -0500

    implemented controllers functionality except for MongoDB related features

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const wordRoutes = require("./routes/words");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(wordRoutes);
 
