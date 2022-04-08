9ca5e69c903f13c9f12cc0cd86804db70f69dd03
Daniel Chang <daniel.changk@gmail.com>
2022-04-08 14:19:19 -0500

    finished api

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const wordRoutes = require("./routes/words");
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.yfvfw.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -10,10 +13,19 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 app.use(wordRoutes);
 
-app.listen(8000);
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(process.env.PORT || 8000);
+  })
+  .catch((err) => console.log(err));
